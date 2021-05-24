08becf909030959e882ca4bd93aaa367a15656b7
zhangwen <zhangwen@hongsong.club>
2021-05-24 15:06:56 +0800

    Http2

--- 计网基础/http2-express/index.js
+++ 计网基础/http2-express/index.js
@@ -8,30 +8,25 @@ const app = express()
 
 
 app.all('*', function (req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    //第二个参数,是一个*号,表示任意域名下的页面都可以都可以请求请求这台服务器;   
-    //设置指定域名:   
-    //res.header("Access-Control-Allow-Origin", "http://baidu.com");    
-    //这样,baidu.com下面的网页,就可以ajax请求你的服务器了    
-    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    // res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    // //第二个参数,为对方可以以哪种HTTP请求方式请求你的服务器,根据自己的情况酌情设置    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     // res.header("X-Powered-By", ' 3.2.1')
     // res.header("Content-Type", "application/json;charset=utf-8")
     next();
 });
 app.use(express.static('./'))
 app.get('/', (req, res) => {
-    res.push("/webp.webp", {method: 'GET'}).end(fs.readFileSync('./test.png'))
+    res.push("/test.png", {method: 'GET'}).end(fs.readFileSync('./test.png'))
     res.end(`<html>
     <head></head>
     <body><img src="/test.png" /></body>
     </html>`)
 })
 
-// app.get('/message', (req, res) => {
-//     res.status(200).json({ message: 'message' })
-// })
+app.get('/message', (req, res) => {
+    res.status(200).json({ message: 'message' })
+})
 
 const options = {
     key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
