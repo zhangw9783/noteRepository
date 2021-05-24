ef890deb9892a463a46ca07c42bc17c005750888
zhangwen <zhangw9783@outlook.com>
2021-05-24 16:17:16 +0800

    Http2 服务端推送

--- 计网基础/http2-express/index.js
+++ 计网基础/http2-express/index.js
@@ -18,9 +18,30 @@ app.all('*', function (req, res, next) {
 app.use(express.static('./'))
 app.get('/', (req, res) => {
     res.push("/test.png", {method: 'GET'}).end(fs.readFileSync('./test.png'))
+    res.push("/message", {method: 'GET'}).end(JSON.stringify({message: 'server push'}))
     res.end(`<html>
     <head></head>
-    <body><img src="/test.png" /></body>
+    <body>
+        <img src="/test.png" />
+        <h1>Http2 api test</h1>
+    <p>Get the message:</p>
+    <button onClick="getmessage()">Get Message</button>
+    <p id="msg"></p>
+    <script>
+        function getmessage() {
+            let http = new XMLHttpRequest()
+            http.onreadystatechange = () => {
+                if (http.readyState === 4)
+                    if (http.status == 200) {
+                        let res = JSON.parse(http.responseText)
+                        document.querySelector('#msg').textContent = "The message from the server is '"+res.message+"'."
+                    }
+            }
+            http.open('get', 'https://localhost:3000/message', true)
+            http.send()
+        }
+    </script>
+    </body>
     </html>`)
 })
 
